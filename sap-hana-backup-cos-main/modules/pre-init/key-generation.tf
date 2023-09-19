0f0be202c9b5f758071c6e18fbbea309a375b0e1
mktse <108746654+mktse@users.noreply.github.com>
2023-09-19 10:44:09 -0500

    Delete sap-hana-backup-cos-main directory
    Updating format

--- sap-hana-backup-cos-main/modules/pre-init/key-generation.tf
+++
@@ -1,8 +0,0 @@
-# Export Terraform variable values to a temp id_rsa file
-resource "local_file" "tf_id_rsa" {
-  content = <<-DOC
-${var.private_ssh_key}
-    DOC
-  filename = "${var.ID_RSA_FILE_PATH}"
-  file_permission = "0600"
-}
--- sap-hana-backup-cos-main/sch.auto.tfvars
+++
@@ -1,3 +0,0 @@
-# Do not edit these variables. These are programatic values for schematics&cli deployments.
-private_ssh_key = "n.a"
-BASTION_FLOATING_IP = "localhost"
